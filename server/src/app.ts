d06782e9283e90962e7c45ae8850802c5b8b2c75
ovan zaimul <ovanzaimul@gmail.com>
2023-12-05 12:42:09 +0700

    update static path url

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 
@@ -6,7 +7,7 @@ import recipesRouter from './routes/recipesRoutes';
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/api/v1/recipes', recipesRouter);
 
