8b9e90d6150b817e30a207243bc772b0241dcc1d
ovan zaimul <ovanzaimul@gmail.com>
2023-12-09 13:24:45 +0700

    handle db connection error

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,4 +19,21 @@ mongoose
     app.listen(port, () => {
       console.log(`app running on port ${port}`);
     });
+  })
+  .catch((err) => {
+    console.error('Connection error🟥:', err);
   });
+
+// Globally handled unhandled rejection and uncaughtException
+// process
+//   .on('unhandledRejection', (reason: Error, promise) => {
+//     console.error({ name: reason.name, message: reason.message });
+//     console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+//     server.close((err) => {
+//       process.exit(1);
+//     });
+//   })
+//   .on('uncaughtException', (err) => {
+//     console.error(err, 'Uncaught Exception thrown');
+//     process.exit(1);
+//   });
