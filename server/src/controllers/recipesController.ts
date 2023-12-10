b984606bdd5c605b1d73d6afe9ba109afee7458e
ovan zaimul <ovanzaimul@gmail.com>
2023-12-10 20:08:15 +0700

    exlude id and __v from return value

--- server/src/controllers/recipesController.ts
+++ server/src/controllers/recipesController.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 
-import Recipe from './../models/recipeModel';
-import catchAsync from '../utils/catchAsync';
 import AppError from '../utils/AppError';
+import catchAsync from '../utils/catchAsync';
+import Recipe from './../models/recipeModel';
 
 export const getAllRecipes = catchAsync(async (req: Request, res: Response) => {
   let recipes = [];
@@ -11,7 +11,7 @@ export const getAllRecipes = catchAsync(async (req: Request, res: Response) => {
   if (searchQuery) {
     recipes = await Recipe.find({
       name: new RegExp(String(searchQuery), 'i'),
-    });
+    }).select('-_id -__v');
   } else {
     recipes = await Recipe.find();
   }
@@ -25,7 +25,11 @@ export const getAllRecipes = catchAsync(async (req: Request, res: Response) => {
 export const createRecipe = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { name, ingredients, instruction } = req.body;
-    const recipe = await Recipe.create({ name, ingredients, instruction });
+    const recipe = await Recipe.create({
+      name,
+      ingredients,
+      instruction,
+    });
 
     if (!recipe) {
       return next(new AppError(`cannot create recipe`, 404));
@@ -42,9 +46,9 @@ export const getRecipe = catchAsync(async (req: Request, res: Response) => {
   const id_or_name = req.params.id_or_name;
   let recipe = null;
   if (mongoose.Types.ObjectId.isValid(id_or_name)) {
-    recipe = await Recipe.findById(id_or_name);
+    recipe = await Recipe.findById(id_or_name).select('-_id -__v');
   } else {
-    recipe = await Recipe.findOne({ name: id_or_name });
+    recipe = await Recipe.findOne({ name: id_or_name }).select('-_id -__v');
   }
 
   res.status(200).json({
@@ -58,7 +62,7 @@ export const updateRecipe = catchAsync(async (req: Request, res: Response) => {
   const recipe = await Recipe.findByIdAndUpdate(id, req.body, {
     new: true,
     runValidators: true,
-  });
+  }).select('-_id -__v');
   res.status(200).json({
     message: 'success',
     recipe,
